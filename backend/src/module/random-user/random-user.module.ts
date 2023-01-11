4432883d42274a5a19bccc14dd211fcda30df5e6
Pedro-Afonso <pedro.766@hotmail.com>
2023-01-11 10:43:43 -0300

    feat: Update controllers

--- backend/src/module/random-user/random-user.module.ts
+++ backend/src/module/random-user/random-user.module.ts
@@ -1,9 +1,11 @@
-import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
-import { RandomUserService } from './random-user.service';
+import { RandomUserController } from './random-user.controller'
+import { HttpModule } from '@nestjs/axios'
+import { Module } from '@nestjs/common'
+import { RandomUserService } from './random-user.service'
 
 @Module({
   imports: [HttpModule],
+  controllers: [RandomUserController],
   providers: [RandomUserService],
 })
 export class RandomUserModule {}
