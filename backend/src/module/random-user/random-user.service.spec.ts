c168efc89e54ca5bfb39016cdcd5854984ed868f
Pedro-Afonso <pedro.766@hotmail.com>
2023-01-11 15:33:52 -0300

    test: Add describe for findAll service method

--- backend/src/module/random-user/random-user.service.spec.ts
+++ backend/src/module/random-user/random-user.service.spec.ts
@@ -1,18 +1,55 @@
+import { HttpService } from '@nestjs/axios'
 import { Test, TestingModule } from '@nestjs/testing'
+import { of } from 'rxjs'
 import { RandomUserService } from './random-user.service'
+import { randomUserStub } from './test/stub/random-user-stub'
 
 describe('RandomUserService', () => {
-  let service: RandomUserService
+  let randomUserService: RandomUserService
+  let httpService: HttpService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RandomUserService],
+      providers: [
+        RandomUserService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+      ],
     }).compile()
 
-    service = module.get<RandomUserService>(RandomUserService)
+    randomUserService = module.get<RandomUserService>(RandomUserService)
+    httpService = module.get<HttpService>(HttpService)
   })
 
   it('should be defined', () => {
-    expect(service).toBeDefined()
+    expect(randomUserService).toBeDefined()
+    expect(httpService).toBeDefined()
+  })
+
+  describe('findAll', () => {
+    it('should return a random user list', async () => {
+      // Arrange
+      const expected = randomUserStub()
+
+      jest.spyOn(httpService, 'get').mockReturnValueOnce(
+        of({
+          status: 200,
+          statusText: 'OK',
+          config: {},
+          headers: {},
+          data: expected,
+        }),
+      )
+
+      // Act
+      const result = await randomUserService.findAll()
+
+      // Assert
+      expect(result).toEqual(expected)
+    })
   })
 })
