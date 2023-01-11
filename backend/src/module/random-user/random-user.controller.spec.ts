f42575af0326698662545d400f3aa42e1a584c92
Pedro-Afonso <pedro.766@hotmail.com>
2023-01-11 10:49:40 -0300

    style: Remove semicolon

--- backend/src/module/random-user/random-user.controller.spec.ts
+++ backend/src/module/random-user/random-user.controller.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { RandomUserController } from './random-user.controller';
+import { Test, TestingModule } from '@nestjs/testing'
+import { RandomUserController } from './random-user.controller'
 
 describe('RandomUserController', () => {
-  let controller: RandomUserController;
+  let controller: RandomUserController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RandomUserController],
-    }).compile();
+    }).compile()
 
-    controller = module.get<RandomUserController>(RandomUserController);
-  });
+    controller = module.get<RandomUserController>(RandomUserController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
--- backend/src/module/random-user/random-user.service.spec.ts
+++ backend/src/module/random-user/random-user.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { RandomUserService } from './random-user.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { RandomUserService } from './random-user.service'
 
 describe('RandomUserService', () => {
-  let service: RandomUserService;
+  let service: RandomUserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [RandomUserService],
-    }).compile();
+    }).compile()
 
-    service = module.get<RandomUserService>(RandomUserService);
-  });
+    service = module.get<RandomUserService>(RandomUserService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
