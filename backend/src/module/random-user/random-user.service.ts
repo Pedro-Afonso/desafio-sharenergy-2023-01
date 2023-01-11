e2a2731e6902e9e9556cc7cc7aad597db5f0079d
Pedro-Afonso <pedro.766@hotmail.com>
2023-01-11 10:40:56 -0300

    feat: Add findAll method

--- backend/src/module/random-user/random-user.service.ts
+++ backend/src/module/random-user/random-user.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { HttpService } from '@nestjs/axios'
+import { lastValueFrom } from 'rxjs'
+import { IRandomUserResponse } from './interfaces/random-user'
 
 @Injectable()
-export class RandomUserService {}
+export class RandomUserService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async findAll(): Promise<IRandomUserResponse> {
+    const url = 'https://randomuser.me/api/?seed=1010&nat=br&results=1'
+    const response = await lastValueFrom(
+      this.httpService.get<IRandomUserResponse>(url, {
+        headers: { 'Accept-Encoding': 'gzip' },
+      }),
+    )
+
+    return response.data
+  }
+}
