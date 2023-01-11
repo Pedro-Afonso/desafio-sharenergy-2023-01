27dc6df11e4b2011e053df167a6822ebe3a406d8
Pedro-Afonso <pedro.766@hotmail.com>
2023-01-11 17:44:13 -0300

    feat: Add 'randomUsers' private variable

--- backend/src/module/random-user/random-user.service.ts
+++ backend/src/module/random-user/random-user.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@nestjs/common'
 import { HttpService } from '@nestjs/axios'
 import { lastValueFrom } from 'rxjs'
-import { IRandomUserResponse } from './interfaces/random-user'
+import { IRandomUser, IRandomUserResponse } from './interfaces/random-user'
 
 @Injectable()
 export class RandomUserService {
   constructor(private readonly httpService: HttpService) {}
 
-  private api = 'https://randomuser.me/api/?seed=1010&nat=br&results=1'
+  private readonly api = 'https://randomuser.me/api/?seed=1010&nat=br&results=1'
 
-  private async fetchRandomUsersFromApi() {
+  private randomUsers: IRandomUser[] = []
+
+  async fetchRandomUsersFromApi(): Promise<IRandomUser[]> {
     const response = await lastValueFrom(
       this.httpService.get<IRandomUserResponse>(this.api, {
         headers: { 'Accept-Encoding': 'gzip' },
       }),
     )
-    return response.data
+
+    this.randomUsers = response.data.results
+
+    return this.randomUsers
   }
 
-  async getRandomUsers(): Promise<IRandomUserResponse> {
+  async getRandomUsers(): Promise<IRandomUser[]> {
     return this.fetchRandomUsersFromApi()
   }
 }
