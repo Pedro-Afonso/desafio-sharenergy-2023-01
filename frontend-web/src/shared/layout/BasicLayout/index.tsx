d149adbce34bbb31ac2b74c5e9b91ae507f3287e
Pedro-Afonso <pedro.766@hotmail.com>
2023-01-12 23:08:29 -0300

    feat: Add BasicLayout layout

---
+++ frontend-web/src/shared/layout/BasicLayout/index.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react'
+
+import './BasicLayout.css'
+
+interface IBasicLayoutProps {
+  aside?: ReactNode
+  toolbar?: ReactNode
+  children: ReactNode
+}
+
+export const BasicLayout: React.FC<IBasicLayoutProps> = ({
+  aside,
+  toolbar,
+  children
+}) => {
+  return (
+    <div className="wrapper centralize">
+      <div>top</div>
+      <div className="container">
+        {aside && <div className="subcontainer-left">{aside}</div>}
+        <div className="subcontainer-right">
+          {toolbar && <div className="toolbar">{toolbar}</div>}
+          <div className="right">{children}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
